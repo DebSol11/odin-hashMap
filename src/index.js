7edd2f7fd077dcc5b610a5e539844bd9b510794b
Michael <100490812+DebSol11@users.noreply.github.com>
2025-12-29 07:36:34 +0400

    Lear how to copy code effectively

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ class HashMap {
   constructor(capacity, loadFactor) {
     // We initialize an array of a specific size to act as our storage "buckets"
     this.hashTable = new Array(capacity).fill(null); // Initialize with null
-    this.capacity = capacity; // also called buckets
+    this.capacity = capacity;
+    this.size = 0; // number of stored unique keys
     this.loadFactor = loadFactor;
   }
 
@@ -16,7 +17,8 @@ class HashMap {
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       // charCodeAt gets the Unicode value of each character
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+      hashCode =
+        (primeNumber * hashCode + key.charCodeAt(i)) % this.hashTable.length;
     }
     return hashCode;
   }
@@ -24,20 +26,22 @@ class HashMap {
   // 2. Set Method: Stores a key-value pair
   set(key, value) {
     const index = this._hash(key);
-    // If the bucket at this index is empty, initialize it as an array (chaining)
+    // If the hashTable at this index is empty, initialize it as an array (chaining)
     if (!this.hashTable[index]) {
       this.hashTable[index] = [];
     }
-
-    // Check if key already exists in the bucket to update it 
-    for (let entry of this.hashTable[index]) {
-      if (entry[0] === key) {
-        entry[1] = value;
+    const hashTable = this.hashTable[index];
+    // Check if key already exists in the hashTable and update it if so
+    for (let i = 0; i < hashTable.length; i++) {
+      if (hashTable[i][0] === key) {
+        hashTable[i][1] = value;
         return;
       }
     }
-    // Otherwise, push new key-value pair into the bucket
-    this.hashTable[index].push([key, value]);
+    hashTable.push([key, value]);
+    this.size++;
+    // check the loadFactor and rehash here
+    // performance increase if further understood: do not rehash but rather dynamically prolonging the array, safes resources
   }
 
   // 3. Get Method: Retrieves a value by its key
@@ -46,21 +50,26 @@ class HashMap {
     const hashTable = this.hashTable[index];
 
     if (hashTable) {
-      for (let entry of hashTable) {
-        if (entry[0] === key) return entry[1];
+      for (let i = 0; i < hashTable.length; i++) {
+        if (hashTable[i][0] === key) {
+          return hashTable[i][1];
+        }
       }
     }
-    return null; // Return undefined if key doesn't exist
+    return null; // Return null if key doesn't exist
   }
 
   has(key) {
-    for (let i = 0; i < this.hashTable.length; i++) {
-      if (this.hashTable === 7) {
-        return true;
-      } else {
-        return false;
+    const index = this._hash(key);
+    const hashTable = this.hashTable[index];
+    if (hashTable) {
+      for (let i = 0; i < hashTable.length; i++) {
+        if (hashTable[i][0] === key) {
+          return true;
+        }
       }
     }
+    return false;
   }
 }
 
@@ -68,8 +77,12 @@ class HashMap {
 const myMap = new HashMap(50, 0.75);
 console.log(myMap);
 console.log("The current capacity of the hashMap is: " + myMap.capacity);
-console.log("The current load capacity is: " + myMap.loadFactor);
-console.log("Extend the capacity of the haspMap when more than " + (Math.ceil(myMap.capacity * myMap.loadFactor)) + " table rows have been started");
+console.log("The current load Factor is: " + myMap.loadFactor);
+console.log(
+  "Extend the capacity of the haspMap when more than " +
+    Math.ceil(myMap.capacity * myMap.loadFactor) +
+    " table rows have been started"
+);
 console.log("The hash value of the name key is: " + myMap._hash("name"));
 myMap.set("name", "Alice");
 console.log(myMap.get("name")); // Output: Alice
